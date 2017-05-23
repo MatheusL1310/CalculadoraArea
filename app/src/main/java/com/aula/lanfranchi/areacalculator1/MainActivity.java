bec24bc66a8f2ea4a95bfaaa35728cd262dd2a60
MatheusL1310 <matheus131097@gmail.com>
2017-05-22 23:49:46 -0300

    Version 1.0

---
+++ app/src/main/java/com/aula/lanfranchi/areacalculator1/MainActivity.java
@@ -0,0 +1,54 @@
+package com.aula.lanfranchi.areacalculator1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import static com.aula.lanfranchi.areacalculator1.R.id.sq;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RadioButton sq;
+    private RadioButton tri;
+    private RadioButton cir;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sq = (RadioButton) findViewById(R.id.sq);
+        tri = (RadioButton) findViewById(R.id.tri);
+        cir = (RadioButton) findViewById(R.id.cir);
+    }
+
+    public void onClickBtnNext(View v) {
+        RadioGroup rd = (RadioGroup) findViewById(R.id.shapes);
+        int checked = rd.getCheckedRadioButtonId();
+        if (checked!=-1){
+            // Check which radio button was clicked
+            switch(checked) {
+                case R.id.sq:
+                    Intent intent = new Intent(this, Tab2Activity.class);
+                    intent.putExtra("shape", sq.getText().toString());
+                    startActivity(intent);
+                    break;
+                case R.id.tri:
+                    Intent intent2 = new Intent(this, Tab2Activity.class);
+                    intent2.putExtra("shape", sq.getText().toString());
+                    startActivity(intent2);
+                    break;
+                case R.id.cir:
+                    Intent intent3 = new Intent(this, Tab2Activity.class);
+                    intent3.putExtra("shape", sq.getText().toString());
+                    startActivity(intent3);
+                    break;
+            }
+        }else {
+            Toast.makeText(this.getApplicationContext(),getString(R.string.error_radiobtn), Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ app/src/main/java/com/aula/lanfranchi/areacalculator1/Tab2Activity.java
@@ -0,0 +1,13 @@
+package com.aula.lanfranchi.areacalculator1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tab2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab2);
+    }
+}
